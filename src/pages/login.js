481bf24408d3214cedc44f39c8b75e6d0375540d
Adi190920 <adiabhang04@gmail.com>
2022-11-04 12:37:00 +0530

    Initial setup

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function Login () {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+    
+        return (
+           <h1>Login Page</h1>
+        );
+    }
---
+++ src/pages/register.js
@@ -0,0 +1,8 @@
+import React, { useState} from "react";
+
+export default function Register() {
+    
+        return (
+            <h1>Register Page</h1>
+        );
+    }
