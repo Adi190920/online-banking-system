4a867fd6b6c88c93dafb46b5fe771ed1ee0b7454
amruthchandra <68228881+amruthchandra@users.noreply.github.com>
2022-11-04 13:04:10 +0530

    Adding Spring Boot Application

---
+++ Backend - SprintBoot/onlinebankingservices/src/main/java/com/project/onlinebankingservices/OnlinebankingservicesApplication.java
@@ -0,0 +1,13 @@
+package com.project.onlinebankingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankingservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebankingservicesApplication.class, args);
+	}
+
+}
---
+++ Backend - SprintBoot/onlinebankingservices/src/test/java/com/project/onlinebankingservices/OnlinebankingservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.onlinebankingservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebankingservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
