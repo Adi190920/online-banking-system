baf62ca4467db02825f5e91fe162669fc3d19028
Adi190920 <65355691+Adi190920@users.noreply.github.com>
2022-11-09 18:29:58 +0530

    Delete Backend - SpringBoot 2.6/onlinebankingservices directory

--- Backend - SpringBoot 2.6/onlinebankingservices/src/main/java/com/project/onlinebankingservices/service/FundTransferdtlsService.java
+++
@@ -1,8 +0,0 @@
-package com.project.onlinebankingservices.service;
-
-import com.project.onlinebankingservices.model.FundTransfer;
-
-public interface FundTransferdtlsService {
-	public String doFundTransfer(FundTransfer ft);
-
-}
